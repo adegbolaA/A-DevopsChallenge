f95ea98a280e24fb7addc2a8e24f5ca2ef52ab5b
EC2 Default User <ec2-user@ip-172-31-4-56.us-east-2.compute.internal>
2023-07-31 09:24:42 +0000

    updates

--- ChallengePackage/demo/src/main/java/com/demo/App.java
+++ ChallengePackage/demo/src/main/java/com/demo/App.java
@@ -3,47 +3,33 @@ package com.demo;
 import java.util.Scanner;
 
 public class App {
+
+    // Helper method to read input safely
+    private static String safeReadLine(Scanner scanner) {
+        return scanner.hasNextLine() ? scanner.nextLine() : "";
+    }
+
     public static void main(String[] args) {
         try {
             Scanner scanner = new Scanner(System.in);
-            System.out.print("Welcome to the Temperature Conversion Problem Checker!\n");
+            System.out.println("Welcome to the Temperature Conversion Problem Checker!");
 
             // Get input from the teacher
             System.out.print("Enter the input numerical value (default: 0): ");
-            double inputNumericalValue;
-            if (scanner.hasNextLine()) {
-                inputNumericalValue = Double.parseDouble(scanner.nextLine());
-            } else {
-                inputNumericalValue = 0.0; // Use a default value if no input provided
-            }
+            double inputNumericalValue = Double.parseDouble(safeReadLine(scanner));
 
             System.out.print("Enter the input unit of measure (Kelvin, Celsius, Fahrenheit, Rankine): ");
-            String inputUnitOfMeasure;
-            if (scanner.hasNextLine()) {
-                inputUnitOfMeasure = scanner.nextLine();
-            } else {
-                inputUnitOfMeasure = "Kelvin"; // Use a default value if no input provided
-            }
+            String inputUnitOfMeasure = safeReadLine(scanner);
 
             System.out.print("Enter the target unit of measure (Kelvin, Celsius, Fahrenheit, Rankine): ");
-            String targetUnitOfMeasure;
-            if (scanner.hasNextLine()) {
-                targetUnitOfMeasure = scanner.nextLine();
-            } else {
-                targetUnitOfMeasure = "Celsius"; // Use a default value if no input provided
-            }
+            String targetUnitOfMeasure = safeReadLine(scanner);
 
             ConversionProblem problem = new ConversionProblem(inputNumericalValue, inputUnitOfMeasure,
                     targetUnitOfMeasure);
 
             // Get student's response
-            System.out.print("Enter the student's numeric response: ");
-            double studentResponse;
-            if (scanner.hasNextLine()) {
-                studentResponse = Double.parseDouble(scanner.nextLine());
-            } else {
-                studentResponse = 0.0; // Use a default value if no input provided
-            }
+            System.out.print("Enter the student's numeric response (default: 0): ");
+            double studentResponse = Double.parseDouble(safeReadLine(scanner));
 
             // Check student's response
             String result = problem.checkResponse(studentResponse);
