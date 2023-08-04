51ce4235236a44e64991a0093ceb38c39a61e3b1
EC2 Default User <ec2-user@ip-172-31-4-56.us-east-2.compute.internal>
2023-08-04 00:32:41 +0000

    adding tffile

--- ekstf_module/variables.tf
+++ ekstf_module/variables.tf
@@ -7,3 +7,13 @@ variable "eks_cluster_name" {
   description = "The name of the EKS cluster."
   default     = "conversionapp-cluster"
 }
+
+variable "access_key" {
+  description = "The AWS access key used to authenticate Terraform."
+  type        = string
+}
+
+variable "secret_key" {
+  description = "The AWS secret key used to authenticate Terraform."
+  type        = string
+}
