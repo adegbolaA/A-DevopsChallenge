f6e34415d99b193c8274546e7f7d04ab1f6793cf
EC2 Default User <ec2-user@ip-172-31-4-56.us-east-2.compute.internal>
2023-08-08 19:17:59 +0000

    eks

--- ChallengePackage/devops-project/src/main/java/com/demo/devopsproject/converter/ConversionProblem.java
+++ ChallengePackage/devops-project/src/main/java/com/demo/devopsproject/converter/ConversionProblem.java
@@ -13,18 +13,16 @@ public class ConversionProblem {
         this.temperatureConverter = temperatureConverter;
     }
 
-    public String checkResponse(double input, String inputUnit, String targetUnit, String studentResponse) {
+    public String checkResponse(double input, String inputUnit, String targetUnit, Object studentResponse) {
         double authoritativeAnswer;
         try {
             authoritativeAnswer = temperatureConverter.convert(input, inputUnit, targetUnit);
-        } catch (NumberFormatException e) {
-            return "Incorrect";
         } catch (IllegalArgumentException e) {
             return "Invalid";
         }
 
-        try {
-            double studentNumericResponse = Double.parseDouble(studentResponse);
+        if (studentResponse instanceof Double) {
+            double studentNumericResponse = (Double) studentResponse;
             double roundedStudentResponse = Math.round(studentNumericResponse * 10.0) / 10.0;
             double roundedAuthoritativeAnswer = Math.round(authoritativeAnswer * 10.0) / 10.0;
 
@@ -33,15 +31,20 @@ public class ConversionProblem {
             } else {
                 return "Incorrect";
             }
-        } catch (NumberFormatException e) {
-            // If the studentResponse is not a valid numeric value, treat it as a string
-            // comparison
-            if (studentResponse.equalsIgnoreCase(String.valueOf(authoritativeAnswer))) {
+        } else if (studentResponse instanceof String) {
+            // If the studentResponse is a string, treat it as a string comparison
+            String studentStringResponse = (String) studentResponse;
+            String roundedAuthoritativeAnswerAsString = String.valueOf(Math.round(authoritativeAnswer * 10.0) / 10.0);
+
+            if (studentStringResponse.equalsIgnoreCase(roundedAuthoritativeAnswerAsString)) {
                 return "Correct";
             } else {
                 return "Incorrect";
             }
+        } else {
+            // If the studentResponse is neither a valid numeric value nor a string,
+            // consider it Invalid
+            return "Invalid";
         }
     }
-
 }
